901ee3b3a451e35f27971da4d9209546384c73fd
Fitriyani <12191238@bsi.ac.id>
2023-01-13 19:22:26 +0700

    Commit ke 2

---
+++ src/main/java/com/juaracoding/fyijavafundamental/ujian/minggu_1/UjianMinggu1.java
@@ -0,0 +1,13 @@
+package com.juaracoding.fyijavafundamental.ujian.minggu_1;
+/*
+Created By IntelliJ IDEA 2022.2.3 (Community Edition)
+Build #IU-222.4345.14, built on October 5, 2022
+@Author Fitriyani a.k.a. Fitriyani
+Java Developer
+Created on 13/01/2023 19:14
+@Last Modified 13/01/2023 19:14
+Version 1.0
+*/
+
+public class UjianMinggu1 {
+}
