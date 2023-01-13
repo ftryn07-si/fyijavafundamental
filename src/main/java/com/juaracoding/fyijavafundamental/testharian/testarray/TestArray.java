c3534c5942f58c09eb7a2897d106ef2faa73dab2
Fitriyani <12191238@bsi.ac.id>
2023-01-13 16:56:40 +0700

    One To Commit

---
+++ src/main/java/com/juaracoding/fyijavafundamental/testharian/testarray/TestArray.java
@@ -0,0 +1,13 @@
+package com.juaracoding.fyijavafundamental.testharian.testarray;
+/*
+Created By IntelliJ IDEA 2022.2.3 (Community Edition)
+Build #IU-222.4345.14, built on October 5, 2022
+@Author Fitriyani a.k.a. Fitriyani
+Java Developer
+Created on 13/01/2023 16:54
+@Last Modified 13/01/2023 16:54
+Version 1.0
+*/
+
+public class TestArray {
+}
